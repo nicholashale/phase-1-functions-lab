357db387bf28068a299fc6170ccfe1c9454631ff
Nicholas Weinberg <nickweinberg@fastmail.com>
2022-03-15 08:39:55 -0700

    commit tree d08c5c91a8e55047efc36524614c924cb7361e6b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet(someValue){
+   return 264 * distanceFromHqInBlocks(someValue);
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+   
+    let distance = distanceTravelledInFeet(start, destination) 
+    
+    if (distance <= 400)
+        return 0;
+    
+    else if (distance > 400 && distance <= 2000)
+        return (.02 * (distance - 400));
+    
+    else if (distance > 2000 && distance <= 2500)
+        return 25;
+    
+    else 
+        return "cannot travel that far";
+}
